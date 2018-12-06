935830c698419e6f116e4d2c4031886ee726b882
Brooke Perry <perrysbrooke@gmail.com>
2018-12-05 20:49:26 -0800

    hw

--- app.rb
+++ app.rb
@@ -1,12 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
-also_reload 'lib/**/*rb'
 require './lib/task'
 require './lib/list'
+also_reload 'lib/**/*rb'
 require 'pg'
 
-require 'rspec'
 require 'pry'
 
+DB = PG.connect({:dbname => "to_do_test"})
+# DB = PG.connect({:dbname => "to_do"}) 
 
-DB = PG.connect({:dbname => "to_do"})
+
+get('/') do
+  @lists = List.all()
+  erb(:index)
+end
+
+get('/lists') do
+  @lists - List.all
+  erb(:lists)
+end
+
+get('/lists/:id') do
+  @list = List.find(params.fetch("id").to_i)
+  erb(:list)
+end
+
+get('/lists/new') do
+  erb(:list_form)
+end
+
+post('/lists') do
+  name = params.fetch("name")
+  list= List.new({:name => name, :id => nil})
+  list.save
+  erb(:list_success)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:task_success)
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ DB = PG.connect({:dbname => 'to_do_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
+     DB.exec("DELETE FROM lists *;")
     DB.exec("DELETE FROM tasks *;")
   end
 end
